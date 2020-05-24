62b68a535437349f77a007d409566fbfcee4a9c7
Ildar Nogmanov <nogmanov@gmail.com>
2020-05-24 16:31:40 +0300

    Add my image

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,30 @@
 import React from "react"
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import Img from "gatsby-image"
+import { graphql } from "gatsby"
 
-const IndexPage = () => (
+const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hello!</h1>
     <p>I am a web developer.</p>
+    <Img fluid={data.file.childImageSharp.fluid}></Img>
   </Layout>
 )
+
+export const query = graphql`
+  query {
+    file(relativePath: { eq: "avatar.jpeg" }) {
+      childImageSharp {
+        # Specify the image processing specifications right in the query.
+        # Makes it trivial to update as your page's design changes.
+        fluid(maxWidth: 460) {
+          ...GatsbyImageSharpFluid
+        }
+      }
+    }
+  }
+`
 
 export default IndexPage
