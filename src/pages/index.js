88c8c0171c695d5cfc1d5b435c838bfdf907d641
Ildar Nogmanov <nogmanov@gmail.com>
2020-05-24 18:42:07 +0300

    Fix image stretching on desktop

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,13 +3,22 @@ import Layout from "../components/layout"
 import SEO from "../components/seo"
 import Img from "gatsby-image"
 import { graphql } from "gatsby"
+import styled from "styled-components"
+
+const ImgContainer = styled.div`
+  @media (min-width: 576px) {
+    width: 400px;
+  }
+`
 
 const IndexPage = ({ data }) => (
   <Layout>
     <SEO title="Home" />
     <h1>Hello!</h1>
     <p>I am a web developer.</p>
-    <Img fluid={data.file.childImageSharp.fluid}></Img>
+    <ImgContainer>
+      <Img fluid={data.file.childImageSharp.fluid}></Img>
+    </ImgContainer>
   </Layout>
 )
 
@@ -19,7 +28,7 @@ export const query = graphql`
       childImageSharp {
         # Specify the image processing specifications right in the query.
         # Makes it trivial to update as your page's design changes.
-        fluid(maxWidth: 460) {
+        fluid(maxWidth: 460, quality: 100) {
           ...GatsbyImageSharpFluid
         }
       }
